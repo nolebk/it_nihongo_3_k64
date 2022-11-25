3be7c981b98f62013be00b6fd407cd588973bda6
tungnguyensnk <tung.nd194714@sis.hust.edu.vn>
2022-11-25 09:55:08 +0700

    update 25/11/2022 1

--- main.c
+++ main.c
@@ -49,7 +49,7 @@ void commit(char *string, int i) {
     char *command = malloc(200);
     sprintf(command, "git commit -m \"update %s\"", tmp);
     system(command);
-    system("git push origin master");
+    system("cd");
     free(tmp);
     tmp = NULL;
 }
